ed6c35e263e31377510c293cf7f39e83b75ad16e
Jorge Amaro <inf.amaro.jc@gmail.com>
2023-08-22 22:17:36 -0300

    Externalizing configuration for test, public profiles, h2 and environment variable.

---
+++ src/main/java/com/devsuperior/bds04/config/AppConfig.java
@@ -0,0 +1,21 @@
+package com.devsuperior.bds04.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+
+@Configuration
+public class AppConfig {
+
+	@Value("${jwt.secret}")
+	private String jwtSecret;
+	
+	@Bean
+	JwtAccessTokenConverter accessTokenConveter() {
+		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+		tokenConverter.setSigningKey(jwtSecret);
+		
+		return tokenConverter;
+	}
+}
